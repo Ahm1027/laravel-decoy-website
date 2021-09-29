db131bd9d964f6d49566a895d57a35e496e81a4f
Ghiyas Ud Din Ahmed <ghiyasuddinahmed@gmail.com>
2021-09-29 11:01:31 +0500

    register added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,81 +20,22 @@
         <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
     </head>
     <body>
-        @yield('content')
-        <footer class="footer pt-100">
-            <div class="container">
-              <div class="row">
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                  <div
-                    class="footer-widget mb-60 wow fadeInLeft"
-                    data-wow-delay=".2s"
-                  >
-                    <a href="index.html" class="logo mb-30"
-                      ><img src="{{ asset('img/logo/logo.svg') }}" alt="logo"
-                    /></a>
-                    <p class="mb-30 footer-desc">
-                      We Crafted an awesome desig library that is robust and intuitive
-                      to use. No matter you're building a business presentation
-                      websit.
-                    </p>
-                  </div>
-                </div>
+      @include('inc.navbar')
+      @yield('content')
+      @include('inc.footer')
       
-                <div class="col-xl-3 col-lg-3 col-md-6 ms-auto">
-                  <div
-                    class="footer-widget mb-60 wow fadeInRight"
-                    data-wow-delay=".8s"
-                  >
-                    <h4>Contact</h4>
-                    <ul class="footer-contact">
-                      <li>
-                        <p>alizah@paymentcompanion.com</p>
-                      </li>
-                      <li>
-                        <p>United State Of America *12 Street House</p>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+      <a href="#" class="scroll-top lni lni-arrow-up">
+      </a>
       
-              <div class="copyright-area">
-                  <div class="footer-social-links">
-                    <ul class="d-flex justify-content-center">
-                      <li>
-                        <a href="javascript:void(0)"
-                        class="lni lni-facebook-filled"></a>
-                      </li>
-                      <li>
-                        <a href="javascript:void(0)"
-                        class="lni lni-twitter-filled"></a>
-                      </li>
-                      <li>
-                        <a href="javascript:void(0)"
-                        class="lni lni-linkedin-original"></a>
-                      </li>
-                      <li>
-                        <a href="javascript:void(0)"
-                        class="lni lni-instagram-filled"></a>
-                      </li>
-                    </ul>
-                </div>
-              </div>
-            </div>
-          </footer>
-      
-          <a href="#" class="scroll-top lni lni-arrow-up">
-          </a>
-      
-          <script src="{{ asset('js/bootstrap.bundle-5.0.0-beta1.min.js') }}"></script>
-          <script src="{{ asset('js/contact-form.js') }}"></script>
-          <script src="{{ asset('js/count-up.min.js') }}"></script>
-          <script src="{{ asset('js/tiny-slider.js') }}"></script>
-          <script src="{{ asset('js/isotope.min.js') }}"></script>
-          <script src="{{ asset('js/glightbox.min.js') }}"></script>
-          <script src="{{ asset('js/wow.min.js') }}"></script>
-          <script src="{{ asset('js/imagesloaded.min.js') }}"></script>
-          <script src="{{ asset('js/main.js') }}"></script>
+      <script src="{{ asset('js/bootstrap.bundle-5.0.0-beta1.min.js') }}"></script>
+      <script src="{{ asset('js/contact-form.js') }}"></script>
+      <script src="{{ asset('js/count-up.min.js') }}"></script>
+      <script src="{{ asset('js/tiny-slider.js') }}"></script>
+      <script src="{{ asset('js/isotope.min.js') }}"></script>
+      <script src="{{ asset('js/glightbox.min.js') }}"></script>
+      <script src="{{ asset('js/wow.min.js') }}"></script>
+      <script src="{{ asset('js/imagesloaded.min.js') }}"></script>
+      <script src="{{ asset('js/main.js') }}"></script>
     </body>
 
 </html>
--- resources/views/web_pages/register.blade.php
+++ resources/views/web_pages/register.blade.php
@@ -1,1 +1,79 @@
-REGISTER PAGE
+@extends('layouts.app')
+
+@section('content')
+
+
+    <section class="inner-page hero-section d-flex aligns-items-center justify-content-center mb-5">
+        <div class="container border border-5 rounded border-info" style="width: 30rem; height: 100%">
+          <form
+            action="#"
+            method="post"
+            style="width: 23rem"
+            class="ms-auto me-auto"
+          >
+            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px">
+              Register
+            </h3>
+
+
+            <div class="form-outline mb-4">
+              <input
+                type="text"
+                name="company"
+                id="form-companyname"
+                class="form-control form-control-lg"
+              />
+              <label class="form-label" for="form-companyname">Company Name</label>
+            </div>
+            <div class="form-outline mb-4">
+                <input
+                  type="file"
+                  name="companyreg"
+                  id="form-companyreg"
+                  class="form-control form-control-lg"
+                />
+                <label class="form-label" for="form-companyreg">Company Reg</label>
+            </div>
+
+            
+            <div class="form-outline mb-4">
+              <input
+                type="text"
+                name="username"
+                id="form-username"
+                class="form-control form-control-lg"
+              />
+              <label class="form-label" for="form-username">User Name</label>
+            </div>
+
+            <div class="form-outline mb-4">
+              <input
+                type="password"
+                name="password"
+                id="form-pass"
+                class="form-control form-control-lg"
+              />
+              <label class="form-label" for="form-pass">Password</label>
+            </div>
+
+            <div class="pt-1 mb-4">
+              <button
+                class="btn btn-info btn-lg btn-block"
+                name="login"
+                value="login"
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+            <p class="small mb-2 pb-lg-2">
+              <a class="text-muted" href="#!">Forgot password?</a>
+            </p>
+            <p>
+              Already have an account?
+              <a href="{{ url('/login') }}" class="link-info">Login here</a>
+            </p>
+          </form>
+        </div>
+      </section>
+    @endsection
