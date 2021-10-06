f5bfb9a6ff6a091fc818452f3d3a70fa45a678e4
Ghiyas Ud Din Ahmed <ghiyasuddinahmed@gmail.com>
2021-10-07 00:52:55 +0500

    added offices page

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,61 @@
+<footer class="footer pt-100">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-3 col-lg-4 col-md-6">
+          <div
+            class="footer-widget mb-60 wow fadeInLeft"
+            data-wow-delay=".2s"
+          >
+            <a href="index.html" class="logo mb-30"
+              ><img src="{{ asset('img/logo/logo.svg') }}" alt="logo"
+            /></a>
+            <p class="mb-30 footer-desc">
+              We Crafted an awesome desig library that is robust and intuitive
+              to use. No matter you're building a business presentation
+              websit.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-3 col-md-6 ms-auto">
+          <div
+            class="footer-widget mb-60 wow fadeInRight"
+            data-wow-delay=".8s"
+          >
+            <h4>Contact</h4>
+            <ul class="footer-contact">
+              <li>
+                <p>alizah@paymentcompanion.com</p>
+              </li>
+              <li>
+                <p>United State Of America *12 Street House</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="copyright-area">
+          <div class="footer-social-links">
+            <ul class="d-flex justify-content-center">
+              <li>
+                <a href="javascript:void(0)"
+                class="lni lni-facebook-filled"></a>
+              </li>
+              <li>
+                <a href="javascript:void(0)"
+                class="lni lni-twitter-filled"></a>
+              </li>
+              <li>
+                <a href="javascript:void(0)"
+                class="lni lni-linkedin-original"></a>
+              </li>
+              <li>
+                <a href="javascript:void(0)"
+                class="lni lni-instagram-filled"></a>
+              </li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
--- resources/views/web_pages/offices.blade.php
+++ resources/views/web_pages/offices.blade.php
@@ -1,67 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<header class="header navbar-area">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-12">
-          <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="{{ url('/') }}">
-              <img src="{{ asset('img/logo/logo.svg') }}" alt="Logo" />
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="toggler-icon"></span>
-              <span class="toggler-icon"></span>
-              <span class="toggler-icon"></span>
-            </button>
-
-            <div
-              class="collapse navbar-collapse sub-menu-bar"
-              id="navbarSupportedContent"
-            >
-              <ul id="nav" class="navbar-nav ms-auto">
-                <li class="nav-item">
-                  <a
-                    class="page-scroll active"
-                    href="{{ url('/') }}"
-                    >Home</a
-                  >
-                </li>
-
-                <li class="nav-item">
-                  <a class="page-scroll" href="{{ url('/offices') }}">Our Offices</a>
-                </li>
-                
-                <li class="nav-item">
-                  <a class="page-scroll" href="{{ url('/contact') }}">Contact</a>
-                </li>
-
-                <div style="border-left:3px solid #000;height:60px"></div>
-
-                <li class="nav-item">
-                  <a class="page-scroll" href="{{ url('/register') }}">Sign Up</a>
-                </li>
-                
-              </ul>
-            </div>
-            <!-- navbar collapse -->
-          </nav>
-          <!-- navbar -->
+  <section class="inner-page hero-section d-flex aligns-items-center justify-content-center">
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-5 contact-item mb-5">
+        <div class="contact-icon">
+          <i class="lni lni-map-marker">Map</i>
+        </div>
+        <div class="contact-content">
+          <h3>Qatar Office(head Office)</h3>
+          <h4>Address</h4>
+          <p>Salwa Ind Area,6998,Doha </p>
+          <p>Qatar</p>
         </div>
       </div>
-      <!-- row -->
+
+
+      <div class="col-md-5 contact-item mb-5">
+          <div class="contact-icon">
+              <i class="lni lni-map-marker">Map</i>
+          </div>
+          <div class="contact-content">
+            <h3>USA Office</h3>
+            <h4>Address</h4>
+            <p>175 5th Ave, New York, NY 10010 </p>
+            <p>United States</p>
+          </div>
+      </div>
+
+      <div class="col-md-1"></div>
+      <div class="col-md-1"></div>
+
+      <div class="col-md-5 contact-item mt-5">
+        <div class="contact-icon">
+            <i class="lni lni-map-marker">Map</i>
+        </div>
+        <div class="contact-content">
+          <h3>UK Office</h3>
+          <h4>Address</h4>
+          <p>72 Crown Street, London, WC1V 6UG </p>
+          <p>United Kindom</p>
+        </div>
+      </div>
+
+      
+
+      <div class="col-md-5 contact-item mt-5">
+        <div class="contact-icon">
+          <i class="lni lni-map-marker">Map</i>
+        </div>
+        <div class="contact-content">
+          <h3>New Zealand Office</h3>
+          <h4>Address</h4>
+          <p>30 Sovereign Court, Terrace End, Palmerston North 4410 </p>
+          <p>New Zealand</p>
+        </div>
+      </div>
     </div>
-    <!-- container -->
-  </header>
-  <section class="inner-page hero-section d-flex aligns-items-center justify-content-center">
-      Offices Page Content Here
   </section>
 @endsection
